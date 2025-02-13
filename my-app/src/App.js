4dd3e76f7bf60f57072ed7e680009e11e4310dc2
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-02-13 20:11:04 +0530

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 // import TailWindProject from "./tailwind-css-project";
 // import CrudOperation from "./crud-operaction-restapi-mockapi";
 // import FireBaseCrud from "./firebase-crud-operaction/index.jsx";
-// import CSSAnimation from "./css-animation";
+import CSSAnimation from "./css-animation";
 function App() {
   return (
     <>
       {/* <CrudOperation /> */}
       {/* <FireBaseCrud /> */}
-      {/* <CSSAnimation /> */}
+      <CSSAnimation />
       {/* <TailWindProject /> */}
     </>
   );
