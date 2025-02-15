f3c4fb5bdffafdcc8c8b02310dc40069ba643943
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-02-15 22:59:39 +0530

    create new nav-bar and footer file

--- my-app/src/starz-web-app/components/Footer/Footer.jsx
+++ my-app/src/starz-web-app/components/Footer/Footer.jsx
@@ -1,8 +1,29 @@
 import "./style.css";
+import firstList from "../../../contents/starz-web-app/home/data";
 export const Footer = () => {
+  const renderFooterList = (item, index) => (
+    <li className="footer-list" key={index}>
+      <a className="footer-link">{item}</a>
+    </li>
+  );
   return (
-    <footer>
-      <h1 className="red">Footer</h1>
+    <footer className="footer-main">
+      <div>
+        <h3 className="footer-heading">Browse STARZ</h3>
+        <ul>
+          {firstList.map(renderFooterList)}
+          {/* last footer list */}
+          <li className="footer-list">
+            <a
+              href="https://www.w3schools.com/TAGs/tryit.asp?filename=tryhtml_link_test"
+              className="footer-link last-footer-list"
+            >
+              More Originals...
+            </a>
+          </li>
+          {/*  */}
+        </ul>
+      </div>
     </footer>
   );
 };
