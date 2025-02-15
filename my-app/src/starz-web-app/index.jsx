cf038ef9401f14648ef45938f5e318998e0e82ff
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-02-15 22:55:39 +0530

    create new nav-bar and footer file

--- my-app/src/starz-web-app/index.jsx
+++ my-app/src/starz-web-app/index.jsx
@@ -1,10 +1,10 @@
 import { Footer } from "./components/Footer/Footer";
-import { NavBar } from "./components/NavBar/NavBar";
+// import { NavBar } from "./components/NavBar/NavBar";
 const StarzWebApp = () => {
   return (
     <section>
-      <NavBar />
-      <h1>StarzWebApp</h1>
+      {/* <NavBar /> */}
+      {/* <h1>StarzWebApp</h1> */}
       <Footer />
     </section>
   );
