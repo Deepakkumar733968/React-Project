32876bd00792559c625b9e2fdd4e7dea42a1cce0
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-02-24 21:47:19 +0530

    changes

--- my-app/src/starz-web-app/components/Footer/Footer.jsx
+++ my-app/src/starz-web-app/components/Footer/Footer.jsx
@@ -16,71 +16,76 @@ export const Footer = () => {
       </a>
     </li>
   );
+  const renderIcons = (item, index) => <div key={index}></div>;
   return (
     <footer className="footer-main">
-      {/* first section */}
-      <div className="first-section">
-        <div>
+      <div className="footer-section-main">
+        {/* first section */}
+        <div className="first-section">
           <h3 className="footer-heading">Browse STARZ</h3>
-          <ul>
-            {firstList.map(renderFooterList)}
-            {/* last footer list */}
-            <li className="footer-list">
-              <a
-                href="https://www.w3schools.com/TAGs/tryit.asp?filename=tryhtml_link_test"
-                className="footer-link last-footer-list"
-              >
-                More Originals...
-              </a>
-            </li>
-            {/*  */}
-          </ul>
+          <div className="first-section-header">
+            <div>
+              <ul>
+                {firstList.map(renderFooterList)}
+                {/* last footer list */}
+                <li className="footer-list">
+                  <a
+                    href="https://www.w3schools.com/TAGs/tryit.asp?filename=tryhtml_link_test"
+                    className="footer-link last-footer-list"
+                  >
+                    More Originals...
+                  </a>
+                </li>
+                {/*  */}
+              </ul>
+            </div>
+            <div>
+              <ul>
+                {secondList.map(renderFooterList)}
+                {/* last footer list */}
+                <li className="footer-list">
+                  <a
+                    href="https://www.w3schools.com/TAGs/tryit.asp?filename=tryhtml_link_test"
+                    className="footer-link last-footer-list"
+                  >
+                    More Movies...
+                  </a>
+                </li>
+                {/*  */}
+              </ul>
+            </div>
+            <div>
+              <ul>
+                {thirdList.map(renderFooterList)}
+                {/* last footer list */}
+                <li className="footer-list">
+                  <a
+                    href="https://www.w3schools.com/TAGs/tryit.asp?filename=tryhtml_link_test"
+                    className="footer-link last-footer-list"
+                  >
+                    More Series...
+                  </a>
+                </li>
+                {/*  */}
+              </ul>
+            </div>
+          </div>
         </div>
-        <div>
-          <ul>
-            {secondList.map(renderFooterList)}
-            {/* last footer list */}
-            <li className="footer-list">
-              <a
-                href="https://www.w3schools.com/TAGs/tryit.asp?filename=tryhtml_link_test"
-                className="footer-link last-footer-list"
-              >
-                More Movies...
-              </a>
-            </li>
-            {/*  */}
-          </ul>
-        </div>
-        <div>
-          <ul>
-            {thirdList.map(renderFooterList)}
-            {/* last footer list */}
-            <li className="footer-list">
-              <a
-                href="https://www.w3schools.com/TAGs/tryit.asp?filename=tryhtml_link_test"
-                className="footer-link last-footer-list"
-              >
-                More Series...
-              </a>
-            </li>
-            {/*  */}
-          </ul>
-        </div>
-      </div>
 
-      {/* second section */}
-      <div className="second-section">
-        <div>
-          <h3 className="footer-heading">Get Started</h3>
-          <ul>{fourthList.map(renderFooterList)}</ul>
-        </div>
-        <div>
-          <h3 className="footer-heading">Help</h3>
-          <ul>{fifthList.map(renderFooterList)}</ul>
-        </div>
-        <div>
-          <h3 className="footer-heading">Company</h3>
-          <ul>{sixthList.map(renderFooterList)}</ul>
+        {/* second section */}
+        <div className="second-section">
+          <div>
+            <h3 className="footer-heading">Get Started</h3>
+            <ul>{fourthList.map(renderFooterList)}</ul>
+          </div>
+          <div>
+            <h3 className="footer-heading">Help</h3>
+            <ul>{fifthList.map(renderFooterList)}</ul>
+          </div>
+          <div>
+            <h3 className="footer-heading">Company</h3>
+            <ul>{sixthList.map(renderFooterList)}</ul>
+          </div>
         </div>
       </div>
     </footer>
